e41453590ea5316175137bd701dce3a7bccc2f05
Ryohei Funamoto <r.funamoto242@gmail.com>
2026-06-12 11:20:43 +0900

    feat: 記事作成機能を実装
    feat: 記事作成機能を実装

--- simple_blog/views.py
+++ simple_blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
 
@@ -26,5 +28,27 @@ def show(request, id):
     return render(
         request,
         'simple_blog/detail.html',
+        context=context,
+    )
+
+@login_required
+def create(request):
+    form = ArticleForm(request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.user = request.user
+            article.save()
+
+            return redirect('simple_blog:show', id=article.id)
+
+    context = {
+        'form': form,
+    }
+
+    return render(
+        request,
+        'simple_blog/create.html',
         context=context,
     )
