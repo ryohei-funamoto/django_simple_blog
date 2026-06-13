7770b54dce7210f2ab43af52db8c532d60d97ec0
Ryohei Funamoto <r.funamoto242@gmail.com>
2026-06-13 10:22:10 +0900

    feat: 記事更新機能を追加
    feat: 記事更新機能を追加

--- simple_blog/views.py
+++ simple_blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from .models import Article
 from .forms import ArticleForm
 
@@ -52,3 +53,28 @@ def create(request):
         'simple_blog/create.html',
         context=context,
     )
+
+@login_required
+def update(request, id):
+    article = get_object_or_404(Article, pk=id)
+    form = ArticleForm(request.POST or None, instance=article)
+
+    if request.user == article.user:
+        if request.method == 'POST':
+            if form.is_valid():
+                form.save()
+
+                return redirect('simple_blog:show', id=article.id)
+
+        context = {
+            'article': article,
+            'form': form,
+        }
+
+        return render(
+            request,
+            'simple_blog/update.html',
+            context=context,
+        )
+    else:
+        raise PermissionDenied
